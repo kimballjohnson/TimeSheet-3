eee425f53b517c4d94938f5958b90dc042777376
Matt Weimer <mweimer85@gmail.com>
2013-06-28 11:51:38 -0400

    fixed changing UI in background thread error, code organization

--- TimeSheet/Services/DataService.cs
+++ TimeSheet/Services/DataService.cs
@@ -15,18 +15,31 @@ namespace TimeSheet.Services
 {
     public class DataService : IDataService
     {
+        #region Public Methods
+
+        public void GetCalendarDays()
+        {
+            BackgroundWorker calendarWorker = new BackgroundWorker();
+            calendarWorker.DoWork += CheckCalendarAsync;
+            calendarWorker.RunWorkerCompleted += CheckCalendarAsyncCompleted;
+            calendarWorker.RunWorkerAsync();
+        }
+
         public void GetChangesets()
         {
             BackgroundWorker changesetsWorker = new BackgroundWorker();
-            changesetsWorker.DoWork += new DoWorkEventHandler(GetChangesetsAsync);
+            changesetsWorker.DoWork += GetChangesetsAsync;
+            changesetsWorker.RunWorkerCompleted += GetChangesetsAsyncCompleted;
             changesetsWorker.RunWorkerAsync();
         }
 
-        public event GetChangesetsCompletedEventHandler GetChangesetsCompleted;
+        #endregion
+
+        #region Private Methods
 
         private void GetChangesetsAsync(object sender, DoWorkEventArgs e)
         {
-            List<ChangesetModel> changesetsForWeek = new List<ChangesetModel>();
+            changesetsForWeek.Clear();
 
             TeamFoundationServer tfs = null;
 
@@ -66,114 +79,132 @@ namespace TimeSheet.Services
                     .Select(cs => new ChangesetModel(cs))
                     .ToList();
             }
-            catch (Exception ex)
-            {
-                if (GetChangesetsCompleted != null)
-                    GetChangesetsCompleted(this, new GetChangesetsCompletedEventArgs() { Error = ex });
-                return;
-            }
             finally
             {
                 if (tfs != null)
                     tfs.Dispose();
             }
-
-            if (GetChangesetsCompleted != null)
-                GetChangesetsCompleted(this, new GetChangesetsCompletedEventArgs() { Changesets = changesetsForWeek });
         }
 
-        public void GetCalendarDays()
+        private void GetChangesetsAsyncCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            BackgroundWorker  calendarWorker = new BackgroundWorker();
-            calendarWorker.DoWork += new DoWorkEventHandler(CheckCalendarAsync);
-            calendarWorker.RunWorkerAsync();
+            if (e.Error == null)
+            {
+                if (GetChangesetsCompleted != null)
+                    GetChangesetsCompleted(this, new GetChangesetsCompletedEventArgs() { Changesets = changesetsForWeek });
+            }
+            else
+            {
+                if (GetChangesetsCompleted != null)
+                    GetChangesetsCompleted(this, new GetChangesetsCompletedEventArgs() { Error = e.Error });
+            }
         }
-
-        public event GetCalendarDaysCompletedEventHandler GetCalendarDaysCompleted;
 
         private void CheckCalendarAsync(object sender, DoWorkEventArgs e)
         {
-            List<CalendarDayModel> calendarDaysForWeek = new List<CalendarDayModel>();
+            calendarDaysForWeek.Clear();
 
-            try
+            using (Microsoft.SharePoint.Client.ClientContext client = new Microsoft.SharePoint.Client.ClientContext(ConfigManager.SharePointWebUrl))
             {
+                var optionsVM = ViewModelLocater.OptionsViewModel;
+                var mainWindowVM = ViewModelLocater.MainWindowViewModel;
 
-                using (Microsoft.SharePoint.Client.ClientContext client = new Microsoft.SharePoint.Client.ClientContext(ConfigManager.SharePointWebUrl))
+                if (optionsVM.SpecifyUserCredentials && optionsVM.CredentialsAreValid)
                 {
-                    var optionsVM = ViewModelLocater.OptionsViewModel;
-                    var mainWindowVM = ViewModelLocater.MainWindowViewModel;
-
-                    if (optionsVM.SpecifyUserCredentials && optionsVM.CredentialsAreValid)
-                    {
-                        NetworkCredential cred = new NetworkCredential(optionsVM.Username, optionsVM.Password, optionsVM.Domain);
-                        client.Credentials = cred;
-                    }
-
-                    List list = client.Web.Lists.GetByTitle(ConfigManager.SharepointCalendarName);
-                    CamlQuery camlQuery = new CamlQuery();
-
-                    camlQuery.ViewXml =
-                        @"<View>
-                    <Query>
-                        <Where>
-                        <And>
-                            <Eq>
-                                <FieldRef Name='Author' LookupId='TRUE' />
-                                <Value Type='Integer'><UserID/></Value>
-                            </Eq>
-                            <Or>
-                                <And>
-                                    <Geq>
-                                        <FieldRef Name='EventDate' />
-                                        <Value IncludeTimeValue='FALSE' Type='DateTime'>" + mainWindowVM.SelectedWeek.StartOfWeek.Date.ToString("o") + @"</Value>
-                                    </Geq>
-                                    <Leq>
-                                        <FieldRef Name='EventDate' />
-                                        <Value IncludeTimeValue='FALSE' Type='DateTime'>" + mainWindowVM.SelectedWeek.EndOfWeek.Date.ToString("o") + @"</Value>
-                                    </Leq>
-                                </And>
-                                <And>
-                                    <Geq>
-                                        <FieldRef Name='EndDate' />
-                                        <Value IncludeTimeValue='FALSE' Type='DateTime'>" + mainWindowVM.SelectedWeek.StartOfWeek.Date.ToString("o") + @"</Value>
-                                    </Geq>
-                                    <Leq>
-                                        <FieldRef Name='EndDate' />
-                                        <Value IncludeTimeValue='FALSE' Type='DateTime'>" + mainWindowVM.SelectedWeek.EndOfWeek.Date.ToString("o") + @"</Value>
-                                    </Leq>
-                                </And>
-                            </Or>
-                        </And>
-                        </Where>
-                    </Query>
-                    <RowLimit>100</RowLimit>
-                    </View>";
-
-                    ListItemCollection listItems = list.GetItems(camlQuery);
-                    client.Load(
-                        listItems,
-                        items => items
-                                        .Include(
-                                            item => item["Title"],
-                                            item => item["EventDate"],
-                                            item => item["EndDate"]));
-                    client.ExecuteQuery();
-
-                    foreach (ListItem listItem in listItems)
-                        calendarDaysForWeek.Add(new CalendarDayModel(listItem));
+                    NetworkCredential cred = new NetworkCredential(optionsVM.Username, optionsVM.Password, optionsVM.Domain);
+                    client.Credentials = cred;
                 }
+
+                List list = client.Web.Lists.GetByTitle(ConfigManager.SharepointCalendarName);
+                CamlQuery camlQuery = new CamlQuery();
+
+                camlQuery.ViewXml =
+                    @"<View>
+                <Query>
+                    <Where>
+                    <And>
+                        <Eq>
+                            <FieldRef Name='Author' LookupId='TRUE' />
+                            <Value Type='Integer'><UserID/></Value>
+                        </Eq>
+                        <Or>
+                            <And>
+                                <Geq>
+                                    <FieldRef Name='EventDate' />
+                                    <Value IncludeTimeValue='FALSE' Type='DateTime'>" + mainWindowVM.SelectedWeek.StartOfWeek.Date.ToString("o") + @"</Value>
+                                </Geq>
+                                <Leq>
+                                    <FieldRef Name='EventDate' />
+                                    <Value IncludeTimeValue='FALSE' Type='DateTime'>" + mainWindowVM.SelectedWeek.EndOfWeek.Date.ToString("o") + @"</Value>
+                                </Leq>
+                            </And>
+                            <And>
+                                <Geq>
+                                    <FieldRef Name='EndDate' />
+                                    <Value IncludeTimeValue='FALSE' Type='DateTime'>" + mainWindowVM.SelectedWeek.StartOfWeek.Date.ToString("o") + @"</Value>
+                                </Geq>
+                                <Leq>
+                                    <FieldRef Name='EndDate' />
+                                    <Value IncludeTimeValue='FALSE' Type='DateTime'>" + mainWindowVM.SelectedWeek.EndOfWeek.Date.ToString("o") + @"</Value>
+                                </Leq>
+                            </And>
+                        </Or>
+                    </And>
+                    </Where>
+                </Query>
+                <RowLimit>100</RowLimit>
+                </View>";
+
+                ListItemCollection listItems = list.GetItems(camlQuery);
+                client.Load(
+                    listItems,
+                    items => items
+                                    .Include(
+                                        item => item["Title"],
+                                        item => item["EventDate"],
+                                        item => item["EndDate"]));
+                client.ExecuteQuery();
+
+                foreach (ListItem listItem in listItems)
+                    calendarDaysForWeek.Add(new CalendarDayModel(listItem));
             }
-            catch (Exception ex)
+        }
+
+        private void CheckCalendarAsyncCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error == null)
             {
                 if (GetCalendarDaysCompleted != null)
-                    GetCalendarDaysCompleted(this, new GetCalendarDaysCompletedEventArgs() { Error = ex });
-                return;
+                    GetCalendarDaysCompleted(this, new GetCalendarDaysCompletedEventArgs() { CalendarDays = calendarDaysForWeek });
             }
-
-            if (GetCalendarDaysCompleted != null)
-                GetCalendarDaysCompleted(this, new GetCalendarDaysCompletedEventArgs() { CalendarDays = calendarDaysForWeek });
+            else
+            {
+                if (GetCalendarDaysCompleted != null)
+                    GetCalendarDaysCompleted(this, new GetCalendarDaysCompletedEventArgs() { Error = e.Error });
+            }
         }
+
+        #endregion
+
+        #region Public Properties
+
+        public event GetChangesetsCompletedEventHandler GetChangesetsCompleted;
+
+        public event GetCalendarDaysCompletedEventHandler GetCalendarDaysCompleted;
+
+        #endregion
+
+        #region Private Fields
+
+        private List<CalendarDayModel> calendarDaysForWeek = new List<CalendarDayModel>();
+
+        private List<ChangesetModel> changesetsForWeek = new List<ChangesetModel>();
+
+        #endregion
+
     }
+
+    #region Event Handlers and Arguments
 
     public delegate void GetChangesetsCompletedEventHandler(object sender, GetChangesetsCompletedEventArgs e);
 
@@ -190,4 +221,6 @@ namespace TimeSheet.Services
         public List<CalendarDayModel> CalendarDays { get; set; }
         public Exception Error { get; set; }
     }
+
+    #endregion
 }
